e68c504e3e7aaeda76b90e942b0f21ef5dfaaaf1
tacticalwildlife <tacticalwildlifecontrol@gmail.com>
2026-06-13 09:31:48 -0500

    Create client-by-name.js

---
+++ api/client-by-name.js
@@ -0,0 +1,42 @@
+export default async function handler(req, res) {
+  const { name } = req.query;
+  
+  if (!name) {
+    return res.status(400).json({ error: 'Name parameter required' });
+  }
+
+  const query = `
+    query {
+      clients(first: 50, filter: { search: "${name}" }) {
+        edges {
+          node {
+            id
+            firstName
+            lastName
+            companyName
+            emails { address }
+            phones { number }
+            addresses { address city state postalCode }
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const response = await fetch('https://api.getjobber.com/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': process.env.JOBBER_ACCESS_TOKEN,
+        'X-JOBBER-GRAPHQL-VERSION': '2026-05-12'
+      },
+      body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data.data.clients.edges);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
