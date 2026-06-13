0c39dbaf34623166970713ecfdbbc927a035740e
tacticalwildlife <tacticalwildlifecontrol@gmail.com>
2026-06-13 09:14:29 -0500

    Create query-jobber.js

---
+++ api/query-jobber.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { query } = req.body;
+  if (!query) {
+    return res.status(400).json({ error: 'Query required' });
+  }
+
+  try {
+    const response = await fetch('https://api.getjobber.com/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': process.env.JOBBER_ACCESS_TOKEN,
+        'X-JOBBER-GRAPHQL-VERSION': '2024-11-20'
+      },
+      body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
